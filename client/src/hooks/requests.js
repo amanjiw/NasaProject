0bb5bf6292f1ec8db18c1a8b22b2914482271e56
amanji <ghaderi.amanj@gmail.com>
2022-06-10 10:49:08 +0430

    client abort misson added

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -37,6 +37,13 @@ async function httpSubmitLaunch(launch) {
 
 async function httpAbortLaunch(id) {
   // TODO: Once API is ready.
+
+  try {
+    return await fetch(`${API_URL}/lunches/${id}`, { method: "delete" });
+  } catch (err) {
+    return { ok: false };
+  }
+
   // Delete launch with given ID.
 }
 
